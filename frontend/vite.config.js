2301d98f4b98171467509487acc023935df8c8e5
AshiniGarusinghe28 <ashinigarusinghe740@gmail.com>
2025-06-13 11:46:15 +0530

    comit changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'https://github.com/AshiniGarusinghe/My-Portfolio.git',
+  base:'/My-Portfolio/',
   plugins: [react(), tailwindcss()],
   resolve:{
     alias:{
